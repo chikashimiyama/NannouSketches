efe258273c9fdb5e4d4d7bd958363376b4c0d64c
Chikashi Miyama <chikashimiyama@gmail.com>
2019-07-29 17:36:57 +0200

    add points

--- points/src/main.rs
+++ points/src/main.rs
@@ -12,55 +12,61 @@ fn main() {
 struct Point {
     x: f32,
     y: f32,
+    x_speed: f32,
+    y_speed: f32,
     size : f32,
     color: Rgb
 }
 
-impl Default for Point {
-    fn default()-> Point {
-        Point {
-            x:0.0, 
-            y:0.0,
-            size: 1.0,
-            color: Rgb::new(0.0, 0.0, 0.0)
-        }
-    }
-}
-
 struct Model {
-    points : [Point; 64]
+    points : Vec<Point>
 }
 
 fn model(_app: &App) -> Model {
 
-    let mut points : [Point; 64] = Default::default();
-    
-    for point in points.iter_mut() {
+    let mut points = Vec::new();
+    let mut rng = rand::thread_rng();
+    let win = _app.window_rect();
 
-        let mut rng = rand::thread_rng();
-
-        point.x = rng.gen_range(-480.0, 480.0);
-        point.y = rng.gen_range(-320.0, 320.0);
-        point.size = rng.gen_range(5.0, 120.0);
-        point.color = Rgb::new(
-            rng.gen_range(0.0, 1.0), 
-            rng.gen_range(0.0, 1.0),
-            rng.gen_range(0.0, 1.0));
+    for _count in 0..100 {
+        let point = Point{
+            x: rng.gen_range(win.left(), win.right()),
+            y: rng.gen_range(win.bottom(), win.top()),
+            x_speed: rng.gen_range(-8.0, 8.0),
+            y_speed: rng.gen_range(-6.0, 6.0),
+            size: rng.gen_range(10.0, 100.0),
+            color: Rgb::new(
+                rng.gen_range(0.0, 1.0), 
+                rng.gen_range(0.0, 1.0),
+                rng.gen_range(0.0, 1.0))
+        };
+        points.push(point);
     }
-
-    let model = Model{ points : points};
-
-    return model;
+    return Model{ points : points };
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
+
+    let win = _app.window_rect();
+
+    for point in _model.points.iter_mut() {
+        point.x += point.x_speed;
+        point.y += point.y_speed;
+
+        if point.x < win.left() || point.x > win.right() {
+            point.x_speed = -point.x_speed;
+        }
+        if point.y < win.bottom() || point.y > win.top() {
+            point.y_speed = -point.y_speed;
+        }
+    }
 }
 
 fn view(_app: &App, _model: &Model, frame: &Frame){
 
     let draw = _app.draw();
+    draw.background().color(WHITE);
     for point in _model.points.iter() {
-
         draw.ellipse()
             .x_y(point.x, point.y)
             .w_h(point.size, point.size)
